46044a2053e15cee109a6ebc65ff7b20587726bc
Mario <mario.klobucaric@gmail.com>
2023-10-17 16:28:10 +0200

    DE - version

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,66 @@
 $(document).ready(function(){
     // Language switching logic
-    $("#germanFlag").click(function() {
+    $("#germanFlag, #de").click(function() {
         window.location.href = 'index_de.html';
     });
 
-    $("#britishFlag").click(function() {
+    $("#britishFlag, #en").click(function() {
         window.location.href = 'index.html';
     });
+
+
+    $("#germanFlagResume, #ResumeDe").click(function() {
+        window.location.href = 'resume_de.html';
+    });
+
+    $("#britishFlagResume, #ResumeEn").click(function() {
+        window.location.href = 'resume.html';
+    });
+
+
+    $("#germanFlagWork, #workDe").click(function() {
+        window.location.href = 'work_de.html';
+    });
+
+    $("#britishFlagWork, #workEn").click(function() {
+        window.location.href = 'work.html';
+    });
+
+
+    $("#germanFlagEducation, #educationDe").click(function() {
+        window.location.href = 'education_de.html';
+    });
+
+    $("#britishFlagEducation, #educationEn").click(function() {
+        window.location.href = 'education.html';
+    });
+
+
+    $("#germanFlagProjects, #projectsDe").click(function() {
+        window.location.href = 'projects_de.html';
+    });
+
+    $("#britishFlagProjects, #projectsEn").click(function() {
+        window.location.href = 'projects.html';
+    });
+
+    $("#germanFlagContact, #contactDe").click(function() {
+        window.location.href = 'contact_de.html';
+    });
+
+    $("#britishFlagContact, #contactEn").click(function() {
+        window.location.href = 'contact.html';
+    });
+
+
+    $("#germanFlagReading, #readingDe").click(function() {
+        window.location.href = 'reading_de.html';
+    });
+
+    $("#britishFlagReading, #readingEn").click(function() {
+        window.location.href = 'reading.html';
+    });
+
+
+
 });
